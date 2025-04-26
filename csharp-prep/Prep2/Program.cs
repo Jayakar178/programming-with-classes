3894cec704f1bc2c2209b6861a4c7d1731e340e9
Jayakar178 <ped24005@byui.edu>
2025-04-26 16:59:16 -0600

    prep 1 and prepe 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,7 +5,44 @@ class Program
     static void Main(string[] args)
     {
         
-        // this prep2//
-        Console.WriteLine("Hello  this is Prep2 ");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string letter;
+
+     
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+       
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class.");
+        }
+        else
+        {
+            Console.WriteLine("Don't worry! Keep working hard and you'll improve next time.");
+        }
     }
 }
