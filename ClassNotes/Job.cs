dc0eb96f07fc2d7361cc4ac2f4986355831fa2b8
Jayakar178 <ped24005@byui.edu>
2025-05-08 23:14:19 -0600

    learning-02

---
+++ ClassNotes/Job.cs
@@ -0,0 +1,12 @@
+public class Job
+{
+    public string JobTitle { get; set; }
+    public string Company { get; set; }
+    public int StartYear { get; set; }
+    public int EndYear { get; set; }
+
+    public void Display()
+    {
+        Console.WriteLine($"{JobTitle} ({Company}) {StartYear} - {EndYear}");
+    }
+}
--- ClassNotes/Program.cs
+++ ClassNotes/Program.cs
@@ -1,13 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-List<string> animals = new List<string>();
+﻿using System;
 
-
-animals.Add("Cow");
-animals.Add("Horse");
-animals.Add("Pig");
-
-foreach (string animal in animals)
+class Program
 {
-    Console.WriteLine(animal);
+    static void Main(string[] args)
+    {
+        Resume myResume = new Resume();
+        myResume.Name = "Jayakar Peddeli";
+
+        Job job1 = new Job();
+        job1.JobTitle = "Software Engineer";
+        job1.Company = "Tech Corp";
+        job1.StartYear = 2020;
+        job1.EndYear = 2022;
+
+        Job job2 = new Job();
+        job2.JobTitle = "Web Developer";
+        job2.Company = "Creative Agency";
+        job2.StartYear = 2022;
+        job2.EndYear = 2024;
+
+        myResume.Jobs.Add(job1);
+        myResume.Jobs.Add(job2);
+
+        myResume.Display();
+    }
 }
