0db850c543360c297ff204068bf375df8261ebf5
Jayakar178 <ped24005@byui.edu>
2025-06-05 23:04:30 -0600

    07

---
+++ prepare/Learning07/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Learning07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            Console.WriteLine(math.GetSummary());
+            Console.WriteLine(math.GetHomeworkList());
+            Console.WriteLine();
+
+            WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            Console.WriteLine(writing.GetSummary());
+            Console.WriteLine(writing.GetWritingInformation());
+        }
+    }
+}
