175400acab04eef4d859562319013e30d13fc64d
Jayakar178 <ped24005@byui.edu>
2025-07-19 23:09:34 -0600

    final

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,32 @@
+public class Event
+{
+    private string _title;
+    private string _description;
+    private string _date;
+    private string _time;
+    private Address _address;
+
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+
+    public virtual string GetStandardDetails()
+    {
+        return $"{_title}\n{_description}\nDate: {_date}\nTime: {_time}\nAddress: {_address.GetFullAddress()}";
+    }
+
+    public virtual string GetFullDetails()
+    {
+        return GetStandardDetails(); // Will be overridden
+    }
+
+    public virtual string GetShortDescription()
+    {
+        return $"{_title} on {_date}";
+    }
+}
--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address addr = new Address("123 Main St", "Rexburg", "ID", "USA");
+
+        Event lecture = new Lecture("C# Seminar", "Learn C# from a pro", "Aug 10", "10:00 AM", addr, "Dr. Smith", 100);
+        Event reception = new Reception("Networking Night", "Meet and greet", "Aug 11", "6:00 PM", addr, "rsvp@example.com");
+        Event outdoor = new OutdoorGathering("Summer Picnic", "Fun in the sun", "Aug 12", "12:00 PM", addr, "Sunny, 75°F");
+
+        List<Event> events = new List<Event> { lecture, reception, outdoor };
+
+        foreach (var e in events)
+        {
+            Console.WriteLine("STANDARD:");
+            Console.WriteLine(e.GetStandardDetails());
+            Console.WriteLine("\nFULL:");
+            Console.WriteLine(e.GetFullDetails());
+            Console.WriteLine("\nSHORT:");
+            Console.WriteLine(e.GetShortDescription());
+            Console.WriteLine("\n----------------------\n");
+        }
     }
 }
